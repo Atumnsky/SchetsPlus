72a0b39dccc3385d714b85a9ad12eaa67fc555b1
Aki_Sora <yuepeng0319@gmail.com>
2026-01-13 15:45:30 +0100

    SchetsEditor copied

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Windows.Forms;
+
+static class Program
+{
+    [STAThreadAttribute]
+    static void Main()
+    {
+        Application.Run(new SchetsEditor());
+    }
+}
